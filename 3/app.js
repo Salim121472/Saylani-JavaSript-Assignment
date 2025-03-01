d7f5fad898ec0b37c7da5f6a29c896325bf53a8e
salim121472 <salimali121472@gmail.com>
2025-03-01 23:33:22 +0500

    add assiment 3 code

---
+++ 3/app.js
@@ -0,0 +1,15 @@
+let Temperatator = +prompt("Enter Weather in Temperatator");
+if (Temperatator >= 16 && Temperatator <= 20) {
+    console.log("Take Care OF Your self");
+}
+else if (Temperatator >= 21 && Temperatator <= 26) {
+    alert("Awesome Weather Now !");
+}
+else if (Temperatator >= 20 && Temperatator <= 100) {
+    alert("Please Do Not Go Out From Your Home !");
+
+}
+else {
+    alert("invlide values");
+
+}
